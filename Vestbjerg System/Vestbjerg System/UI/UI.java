cb7c7d3160d6bf89f90963a67398d4d1fe2cb854
simonrhiger <10666019@ucn.dk>
2025-06-04 11:21:49 +0200

    Scanner og fejlhåndtering

--- Vestbjerg System/Vestbjerg System/UI/UI.java
+++ Vestbjerg System/Vestbjerg System/UI/UI.java
@@ -18,7 +18,6 @@ public class UI {
         orderController = new OrderController();
         productList = new ArrayList<>();
         customerList = new ArrayList<>();
-        scanner = new Scanner(System.in);
     }
     
     public void makeNewOrder() {       
@@ -28,9 +27,11 @@ public class UI {
         while(addingProducts) {
             searchProducts();
             addProductToOrder();
-            System.out.println("Vil du tilføje flere produkter til ordre ja/nej");
-            String svar = scanner.nextLine().toLowerCase();
-            if(!svar.equals("ja")) {
+            System.out.println("Vil du tilføje flere produkter til ordren?");
+            System.out.println("0) Ja");
+            System.out.println("1) Nej");
+            int svar = forceInt(1);
+            if(svar == 1) {
                 addingProducts = false;
             }
         }
@@ -41,13 +42,15 @@ public class UI {
     }
     
     public void searchProducts() {
-        System.out.println("Hvilke produkt søger du");
-        String searchString = scanner.nextLine().toLowerCase();
         productList.clear();
+        System.out.println("Hvilket produkt søger du?");
+        scanner = new Scanner(System.in);
+        String searchString = scanner.nextLine().toLowerCase();
         productList = orderController.searchProducts(searchString);
         
         if (productList.isEmpty()) {
-            System.out.println("Ingen produkter fundet med søgeordet: " + searchString);
+            System.out.println("Ingen produkter fundet med søgeordet: " + searchString + " , prøv igen.");
+            searchProducts();
         } else {
             System.out.println("Produkter fundet:");
             int i = 0;
@@ -55,16 +58,17 @@ public class UI {
                 System.out.println(i + ") " + productList.get(i).getName());
                 i++;
             }
-        }
-  
+        }  
     }
     
     public void addProductToOrder() {
-        System.out.println("Hvilke produkt vil du tilføje til ordre");
-        int product = scanner.nextInt();
+        System.out.println("Hvilke produkt vil du tilføje til ordre");        
+        SimpleProduct product = productList.get(forceInt(productList.size() - 1));
+        
         System.out.println("Hvor mange stk.?");
-        int quantity = scanner.nextInt();
-        orderController.addProduct(productList.get(product), quantity);
+        int quantity = forceInt(99);
+        
+        orderController.addProduct(product, quantity);
     }
     
     public void showOrderList() {
@@ -72,15 +76,19 @@ public class UI {
     }
     
     public void searchCustomers() {
-        System.out.println("Indtast kunde navn.");
-        String searchString = scanner.nextLine().toLowerCase();
         customerList.clear();
+        System.out.println("Indtast kunde navn.");
+        scanner = new Scanner(System.in);
+        String searchString = scanner.nextLine().toLowerCase();        
         customerList = orderController.searchCustomer(searchString);
         
         if (customerList.isEmpty()) {
-            System.out.println("Ingen kunder fundet med søgeordet: " + searchString);
+            System.out.println("Ingen kunder fundet med søgeordet: " + searchString + " , prøv igen.");
+            searchCustomers();
         } else {
+            System.out.println("Kunder fundet:");
             int i = 0;
+            
             while (i < customerList.size()) {
                 System.out.println(i + ") " + customerList.get(i).getName());
                 i++;
@@ -90,15 +98,43 @@ public class UI {
     
     public void selectCustomer() {
         System.out.println("Hvilke kunde vil du tilføje til ordren?");
-        int customer = scanner.nextInt();
-        orderController.selectCustomer(customerList.get(customer));
+        Customer customer = customerList.get(forceInt(customerList.size() - 1));
+        orderController.selectCustomer(customer);
     }
     
     public void confirmOrder() {
-        System.out.println("Bekræft ordre ja/nej");
-        String svar = scanner.nextLine().toLowerCase();
-        if(!svar.equals("ja")) {
-            orderController.confirmOrder();
+        System.out.println("Bekræft ordre.");
+        System.out.println("0) Ja");
+        System.out.println("1) Nej");
+        
+        switch (forceInt(1)) {
+            case 0:
+                orderController.confirmOrder();
+                System.out.println("Ordre bekræftet. Tak for denne gang.");
+                break;
+            case 1:
+                System.out.println("Ordre annulleret. Tak for denne gang.");
+                break;
+            }
+    }
+    
+    public int forceInt(int max) {
+        int result = 0;
+        scanner = new Scanner(System.in);
+        
+        while (!scanner.hasNextInt() ) 
+        {        
+            scanner.next();
+            System.out.println("Skriv et tal.");
         }
+        
+        result = scanner.nextInt();
+        
+        if (result < 0 || result > max) {
+            System.out.println("Tal ikke gyldigt, prøv igen.");
+            result = forceInt(max);
+        }
+        
+        return result;
     }
 }
